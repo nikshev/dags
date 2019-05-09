ac38dcb1a7e529a6037f8121f4af82aaec324d6a
nikshev81 <nikshev81@gmail.com>
2019-05-09 15:09:25 +0300

    Rates to BigQuery

--- dags/crypto.py
+++ dags/crypto.py
@@ -54,7 +54,6 @@ def crypto_pull_rates():
 dag = DAG('crypto', description='Pull crypto rates from coinmarketcap.com',
           schedule_interval='*/1 * * * *',
           email=['nikshev81@gmail.com'],
-          email_on_failure=True,
           catchup=False)
 
 
