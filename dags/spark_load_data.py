34706a792e087545a082f61a3d27d62b587bb351
perepecha <35768388+perepecha@users.noreply.github.com>
2019-05-12 23:20:23 +0300

    test cluster

--- dags/spark_load_data.py
+++ dags/spark_load_data.py
@@ -20,17 +20,19 @@ dag = DAG(
     default_args=args,
     schedule_interval='@hourly')
 
-new_cluster = {
-    'spark_version': '4.0.x-scala2.11',
-    'node_type_id': 'i3.xlarge',
-    'aws_attributes': {
-        'availability': 'ON_DEMAND'
-    },
-    'num_workers': 1
-}
+#new_cluster = {
+#    'spark_version': '4.0.x-scala2.11',
+#    'node_type_id': 'i3.xlarge',
+#    'aws_attributes': {
+#        'availability': 'ON_DEMAND'
+#    },
+#    'num_workers': 1
+#}
+
+#cluster = {'existing_cluster_id': 'airflow-test'}
 
 notebook_task_params = {
-    'new_cluster': new_cluster,
+    'existing_cluster_id': 'airflow-test',
     'notebook_task': {
         'notebook_path': '/Users/perepe4a@gmail.com/spark-load-data',
     },
